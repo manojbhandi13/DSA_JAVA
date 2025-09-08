19693863c9a1c70a4d30d09da797f9b7d01cf5bc
manojbhandi13 <alphaleadsalpha@gmail.com>
2025-09-08 23:03:09 +0530

    Renamed the class

--- PATTERNS/pattern_11.java
+++ PATTERNS/pattern_11.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class pattern11{
+public class pattern_11{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
