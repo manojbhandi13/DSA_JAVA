13f594e22160c315a1deb46aad594c312b05788d
manojbhandi13 <alphaleadsalpha@gmail.com>
2025-09-09 18:04:42 +0530

    Triangular & summetrical pattern of Alphabet letters

---
+++ PATTERNS/pattern_17.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class pattern_17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            //space
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            //char
+            char ch='A';
+            for(int j=0; j<((2*i+1)/2); j++){
+                System.out.print(ch);
+                ch++;
+            }
+            for(int j=0; j<i+1; j++){
+                System.out.print(ch);
+                ch--;
+            }
+            //space
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
