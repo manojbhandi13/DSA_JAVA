5aef598215dd242f3710952aa70165aef02f1d9c
manojbhandi13 <alphaleadsalpha@gmail.com>
2025-09-11 17:22:45 +0530

    Comment

--- PATTERNS/pattern_19.java
+++ PATTERNS/pattern_19.java
@@ -4,7 +4,7 @@ public class pattern_19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int iniSp=0;
+        int iniSp=0; //Space repition
         for(int i=0; i<n; i++){
             //stars
             for(int j=0; j<n-i; j++){
