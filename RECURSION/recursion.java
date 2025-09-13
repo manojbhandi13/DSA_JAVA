03f1d41d28e1e8f7cd60a25f98c996be5fb5f3e6
manojbhandi13 <alphaleadsalpha@gmail.com>
2025-09-13 22:46:44 +0530

    Basic recursive function with base case

--- RECURSION/recursion.java
+++ RECURSION/recursion.java
@@ -1,10 +1,10 @@
 public class recursion {
-    static int count=0;
+    static int count=0; // global variable
     public static void recurse(){
-    if(count==4) return;
+    if(count==4) return; //base condition
     System.out.println(count);
     count++;
-    recurse();
+    recurse(); // function calls itself
     }
     public static void main(String[] args) {
         recurse();
