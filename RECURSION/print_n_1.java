23fe88a27809f764b51e34cb6543c6ae7cbbf804
manojbhandi13 <alphaleadsalpha@gmail.com>
2025-09-14 11:31:23 +0530

    Print N to 1

--- RECURSION/print_n_1.java
+++ RECURSION/print_n_1.java
@@ -1,21 +1,6 @@
 import java.util.Scanner;
 
 public class print_n_1 {
-    public static void recurse(int n){
-        if(n==0) return;
-        System.out.println(n);
-        recurse(n-1);
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        recurse(n);
-    }
-}
-
-/*import java.util.Scanner;
-
-public class print_n_1 {
     public static void recurse(int i, int n){
         if(i<1) return;
         System.out.println(i);
@@ -25,5 +10,20 @@ public class print_n_1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         recurse(n, n);
+    }
+}
+
+/*import java.util.Scanner;
+
+public class print_n_1 {
+    public static void recurse(int n){
+        if(n==0) return;
+        System.out.println(n);
+        recurse(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        recurse(n);
     }
 }*/
